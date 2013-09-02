419cc7da7771c3da0c5d5182297aacbe7736e1bd
Matt Mueller <mattmuelle@gmail.com>
2013-09-01 18:04:28 -0700

    added bundles

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -38,3 +38,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Don't let npm fuck up timemachine
+tmutil addexclusion /usr/local/lib/node_modules
