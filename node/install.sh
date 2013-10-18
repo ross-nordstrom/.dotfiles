2ae4e40e78be8ebda2ea87862050dc872ca6880d
Matthew Mueller <mattmuelle@gmail.com>
2013-10-18 14:52:06 -0700

    updates for cask

---
+++ node/install.sh
@@ -0,0 +1,4 @@
+npm install -g component
+npm install -g node-dev
+npm install -g serve
+npm install -g node-gyp
--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,8 @@
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vundle=~/.vim/bundle/vundle
+
+if [ ! -d "$vundle" ]; then
+  git clone https://github.com/gmarik/vundle.git $vundle 
+fi
+
 vim +BundleInstall +qall
 
